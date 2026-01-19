3b75b421498237251947f6fabfcde342aef28bf2
Abhijit Sawant <amoghsingh75@gmail.com>
2026-01-19 11:59:37 +0530

    updated BrowserRouter to HashRouter to fix project load issue on gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </>
   );
 }
